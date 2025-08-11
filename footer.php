d124acca1740c791d29f80e4a665086d52c7a7e5
ragrima7 <ragrima7@gmail.com>
2025-08-11 15:18:39 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,33 @@
+<?php
+// includes/footer.php
+?>
+<footer class="site-footer">
+  <!-- フッターリンク -->
+  <div class="footer-links">
+    <a href="faq.php">よくある質問</a>
+    <a href="contact.php">お問い合わせ</a>
+    <a href="policy.php">当サイトのポリシー</a>
+  </div>
+
+  <!-- ロゴ -->
+  <div class="footer-logo">
+    <a href="index.php"><img src="images/logo.svg" alt="C.C.Donuts ロゴ"></a>
+  </div>
+
+  <!-- SNSアイコン -->
+  <div class="sns-icons">
+    <a href="#"><img src="images/twitter.png" alt="Twitter"></a>
+    <a href="#"><img src="images/facebook.png" alt="Facebook"></a>
+    <a href="#"><img src="images/insta.png" alt="Instagram"></a>
+  </div>
+
+  <!-- コピーライト -->
+  <p class="copyright">
+    &copy; <?php echo date('Y'); ?> C.C.Donuts
+  </p>
+</footer>
+
+<script src="/ccdonuts/scripts/main.js"></script>
+<script src="/ccdonuts/scripts/drawer.js"></script>
+</body>
+</html>
